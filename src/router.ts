ad6562ebdb6a809146ec06bdf7f6c34140e778f9
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2023-02-21 17:00:21 -0300

    Create body validator middleware

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { errorValidator } from "./modules/validator";
 
 const router = Router();
 /**
@@ -12,7 +14,12 @@ router.get("/product/:id", (req, res) => {});
 
 router.post("/product", (req, res) => {});
 
-router.put("/product/:id", (req, res) => {});
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  errorValidator,
+  (req, res) => {}
+);
 
 router.delete("/product/:id", (req, res) => {});
 
